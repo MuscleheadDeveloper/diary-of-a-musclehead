a5f2e379d51388d7a05081d05a63236a0c58ad1d
muscleheadDeveloper <njokua04@gmail.com>
2022-11-09 12:30:52 +0100

    new blogg added

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home ({posts}) {
 
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <PostWidget />
+            <PostWidget /> {" "}
             <Categories />{" "}
           </div>
         </div>
